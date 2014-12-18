7b59365af56bb7a64e5480c8de28da3c110369ba
xzdbd <xzdbd@sina.com>
2014-12-18 19:01:52 +0800

    add router

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"github.com/xzdbd/weixingis/controllers"
 	"github.com/astaxie/beego"
+	"github.com/xzdbd/weixingis/controllers"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
+	beego.Router("/weixin", &controllers.MainController{})
 }
