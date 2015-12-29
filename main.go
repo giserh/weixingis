d63581f0238542595bc2e9c7a6168460a76d8a5c
xzdbd <xzdbd@sina.com>
2015-12-29 09:44:18 +0000

    add smtp log

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	beego.SetLogger("smtp", `{"username":"weixingis@163.com","password":"gjfeixphowfkqhlb","host":"smtp.163.com:25","fromAddress":"weixingis@163.com","subject":"Log from weixingis","sendTos":["xzdbd@sina.com"]}`)
 	beego.Info("come in")
 	beego.Run()
 }
